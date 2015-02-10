85bb9c92e2a6b1df870da63f380d381b31c5328e
Davis Cook <daviscook447@gmail.com>
2015-02-09 21:52:58 -0500

    plain data pointers are working

--- objective-fire.js
+++ objective-fire.js
@@ -17,6 +17,7 @@ angular.module('objective-fire', ['firebase'])
       return this.objectConstructorsNew[name];
     },
     registerObjectType: function(schema) {
+      var selfObjF = this;
       var properties = schema.getProperties();
       var pointersData = schema.getPointersData();
       var pointersList = schema.getPointersList();
@@ -26,7 +27,7 @@ angular.module('objective-fire', ['firebase'])
           var deffered = $q.defer();
           if (name in pointersData) {
             this.$loaded().then(function(self) {
-              self[name] = constructorOf(pointersData[name].object)(this._pointers[name]);
+              self[name] = this.constructorOf(pointersData[name].object)(this.pointers[name]);
               deffered.resolve(self.userD);
             });
           } else if (name in pointerList) {
@@ -41,7 +42,21 @@ angular.module('objective-fire', ['firebase'])
           if (self == null) {
             self = {};
           }
-          //TODO: recreate pointerss
+          for (param in self.pointers) { // check every pointer and update the object with the pointers
+            if (typeof self.pointers[param] == "object") { // if its an object it is a list
+
+            } else if (typeof self.pointers[param] != undefined) { // its just a single pointer
+              if (this.pointers && this.pointers[param] != self.pointers[param]) { // something changed
+                console.log("pointer of " + self.pointers[param]);
+                console.log("object type = " + pointersData[param].object);
+                self[param] = new (selfObjF.constructorOf.call(selfObjF, pointersData[param].object))(self.pointers[param]); // set the value that the pointer is bound to to an object
+              } else {
+                self[param] = this[param];
+              }
+            } else {
+              // something should not happen
+            }
+          }
           for (param in self) {
             this[param] = self[param];
           }
@@ -53,18 +68,21 @@ angular.module('objective-fire', ['firebase'])
       this.objectConstructors[schema.name] = function(id) {
         var ref = rootRef.child(schema.loc).child(id);
         var sync = $firebase(ref, { objectFactory: factory });
-        return sync.$asObject();
+        var obj = sync.$asObject();
+        obj.pointers = {};
+        return obj;
       };
       console.log("registering constructor for new " + schema.name);
       this.objectConstructorsNew[schema.name] = function() {
         var ref = rootRef.child(schema.loc).push(); // create a new location for the object we are making
         var sync = $firebase(ref, { objectFactory: factory });
         var obj = sync.$asObject();
+        obj.pointers = {};
         if (schema.objectConstructor != null) {
           schema.objectConstructor.apply(obj, arguments); // call the "mock" constructor
         }
         obj.$save(); // save the new constructed object
-        return sync.$asObject();
+        return obj;
       }
     }
   };
--- tests/test01.js
+++ tests/test01.js
@@ -1,32 +1,44 @@
 angular.module('test01', ['objective-fire'])
 
+// this is a really bad type of test
+
 .controller('ctrl', function($scope, Schema, SchemaUtil, ObjectFire) {
   console.log("doing test01");
+  var ref = new Firebase("https://idea0.firebaseio.com");
   var user = new Schema("user", "users");
   user.addProperty("screenName", SchemaUtil.createData("string"));
   var idea = new Schema("idea", "ideas");
   idea.addProperty("title", SchemaUtil.createData("string"));
   idea.addProperty("description", SchemaUtil.createData("string"));
+  idea.addProperty("comments", SchemaUtil.createDataPointer("comment", true));
   idea.addProperty("owner", SchemaUtil.createDataPointer("user", false));
   idea.setConstructor(function(title, description) {
     this.title = title;
     this.description = description;
+    var auth = ref.getAuth();
+    if (auth) {
+      this.pointers.owner = auth.uid;
+    }
   });
   console.log("property list of idea: " + JSON.stringify(idea.getProperties()));
   console.log("pointer list of idea: " + JSON.stringify(idea.getPointersData()));
   console.log(ObjectFire);
-  var objFire = new ObjectFire(new Firebase("https://idea0.firebaseio.com"));
+  var objFire = new ObjectFire(ref);
   objFire.registerObjectType(idea);
   objFire.registerObjectType(user);
   var User = objFire.constructorOf("user");
   var Idea = objFire.constructorOf("idea");
   var IdeaNew = objFire.constructorOfNew("idea");
-  var testIdea = new Idea("-JhfuxKt7IYKzKN3rHRQ");
-  testIdea.$loaded().then(function(self) {
-    console.log("idea loaded");
-  });
-  var testIdea2 = IdeaNew("test title", "test description");
-  testIdea2.$loaded().then(function(self) {
+  var testUser = new User("simplelogin:12");
+  testUser.$loaded().then(function(self) {
+    console.log("user ");
     console.log(self);
+  })
+  var testIdea = new Idea("-JhlLResumfnaJbf94gk");
+  testIdea.$loaded().then(function(self) {
+    console.log("idea ");
+    console.log(self);
+    console.log("idea user ");
+    console.log(self.owner);
   });
 });
