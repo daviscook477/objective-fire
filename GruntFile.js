1aa294821f958db2711c13bb7012d7e05d1eaa97
Davis Cook <daviscook447@gmail.com>
2015-02-10 12:01:17 -0500

    setting up project with unit testing

---
+++ GruntFile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'), // the package file to use
+    qunit: {
+      all: ['tests/*.html']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.registerTask('default', ['qunit']);
+};
