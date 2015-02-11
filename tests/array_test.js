1c4f93516c938e1392794e5f48ecc8e8059cf7ee
Davis Cook <daviscook447@gmail.com>
2015-02-10 19:35:19 -0500

    corrected unit test for pointer-array, it works now

--- tests/array_test.js
+++ tests/array_test.js
@@ -4,10 +4,10 @@ var injector = angular.injector(['ng', 'objective-fire']);
 var pa = injector.get('PointerArray');
 var s = injector.get('Schema');
 var ObjectFire = injector.get('ObjectFire');
-asyncTest("pointer array works", function(assert) {
+test("pointer array works", function(assert) {
   //assert.expect(0);
   var ref = new Firebase("https://idea0.firebaseio.com");
-  var catSchema = new s('category', 'categories');
+  var catSchema = new s('category', 'ideas');
   catSchema.addDataProperty("title", "string");
   var objFire = new ObjectFire(ref);
   objFire.registerObjectClass(catSchema);
@@ -18,13 +18,10 @@ asyncTest("pointer array works", function(assert) {
   mypa.$loaded().then(function(self) {
     console.log("hello!")
     console.log("hello world ");
-    console.log(mypa[0].title);
-    for (var i = 0; i < mypa.length; i++) {
-      for (param in mypa[i]) {
-        console.log(param, mypa[i][param]);
-      }
-    }
-    assert.ok(mypa);
+    mypa[0].$loaded(function( self2) {
+      console.log(self2.title);
+    });
+    assert.ok(true);
   })
 
 
