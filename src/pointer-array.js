8d4e623ec7d79d1e265fc4a8a804481b37623b0c
Davis Cook <daviscook447@gmail.com>
2015-02-22 18:31:52 -0500

    wrote some comments in the code that documents what work needs to get done

--- src/pointer-array.js
+++ src/pointer-array.js
@@ -5,11 +5,9 @@ angular.module('objective-fire')
   var getFactory = function(fireObject) {
     return $FirebaseArray.$extendFactory({
       $$added: function(snapshot) {
-        console.log("Data obtained for update us " + JSON.stringify(snapshot.val()));
         return fireObject.instance(snapshot.key());
       },
       $$updated: function(snapshot) {
-        console.log("Data obtained for update us" + JSON.stringify(snapshot.val()));
         this.$getRecord(snapshot.key()) = fireObject.instance(snapshot.key());
         return true;
       }
@@ -17,7 +15,6 @@ angular.module('objective-fire')
   };
 
   return function(pointerRef, fireObject) {
-    console.log("creating pointer array");
     var sync = $firebase(pointerRef, { arrayFactory: getFactory(fireObject) });
     var obj = sync.$asArray();
 
