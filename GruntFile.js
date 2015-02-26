e8b9a19b155c4cb71124778ecb60739631d3863e
Davis Cook <daviscook447@gmail.com>
2015-02-26 16:27:11 -0500

    added grunt-contrib-yuidoc for documentation - will be using it to document code from now on

--- GruntFile.js
+++ GruntFile.js
@@ -7,10 +7,24 @@ module.exports = function(grunt) {
     watch: {
       files: ['tests/*.js', 'tests/*.html', 'src/*.js'],
       tasks: ['qunit']
+    },
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: 'src/',
+          outdir: 'build/'
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
   grunt.registerTask('test', ['qunit', 'watch']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('build', ['yuidoc']);
+  grunt.registerTask('default', ['test', 'build']);
 };
