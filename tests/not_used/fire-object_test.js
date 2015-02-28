130d60e1366bc8f9bac6cfb4dddd006b567188e7
Davis Cook <daviscook447@gmail.com>
2015-02-27 23:09:39 -0500

    removed old and unused test cases

--- tests/not_used/fire-object_test.js
+++
@@ -1,9 +0,0 @@
-module("Fire Object Tests");
-
-// obtain angular stuff for testing
-var injector = angular.injector(['ng', 'objective-fire']);
-var FireObject = injector.get('FireObject');
-
-QUnit.test("objective fire conforms to api", function(assert) {
-  //TODO: test fire object
-});
